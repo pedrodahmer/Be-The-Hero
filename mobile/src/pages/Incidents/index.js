a432419028cb0ba06a8d1b72243cfbe39d303126
Pedro Dahmer <pedrohd1213@gmail.com>
2020-03-27 15:22:24 -0300

    mobile do app

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -32,7 +32,7 @@ export default function Incidents(){
 
         setLoading(true)
         
-        const response = await api.get(`incidents`)
+        const response = await api.get('incidents')
 
         setIncidents([... incidents, ...response.data])
         setTotal(response.headers['X-Total-Count'])
